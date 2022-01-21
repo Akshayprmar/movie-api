36044ae35d9743f2357936d7d521dbd1e22a2590
Akshayprmar <30464870+Akshayprmar@users.noreply.github.com>
2022-01-21 19:12:33 +0530

    Add files via upload

---
+++ src/db/films.js
@@ -0,0 +1,261 @@
+export const movies = [{
+        "id": 0,
+        "name": "Avengers: Endgame",
+        "descripton": "After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to undo Thanos' actions and restore order to the universe.",
+        "genre": "Action, Adventure, Fantasy, Sci-Fi",
+        "rating": 8.4,
+        "year": "2019",
+        "country":"USA",
+        "photo": "https://m.media-amazon.com/images/M/MV5BNGZiMzBkZjMtNjE3Mi00MWNlLWIyYjItYTk3MjY0Yjg5ODZkXkEyXkFqcGdeQXVyNDg4NjY5OTQ@._V1_SX300.jpg"
+      },
+      {
+        "id": 1,
+        "name": "The Godfather",
+        "descripton": "Don Vito Corleone, head of a mafia family, decides to hand over his empire to his youngest son Michael. However, his decision unintentionally puts the lives of his loved ones in grave danger.",
+        "genre": "Crime, Drama",
+        "rating": 9.1,
+        "year": "1972",
+        "country":"INDIA",
+        "photo": "https://images.app.goo.gl/5pC3DXs4R6PMfRkW8"
+      },
+      {
+        "id": 2,
+        "name": "The Dark Knight",
+        "descripton": "After Gordon, Dent and Batman begin an assault on Gotham's organised crime, the mobs hire the Joker, a psychopathic criminal mastermind who offers to kill Batman and bring the city to its knees.",
+        "genre": "Action, Adventure",
+        "rating": 9,
+        "year": "2008",
+        "country":"USA",
+        "photo": "https://images.app.goo.gl/oLYNmMSVKv3jF84B6"
+      },
+      {
+        "id": 3,
+        "name": "Pulp Fiction",
+        "descripton": "In the realm of underworld, a series of incidents intertwines the lives of two Los Angeles mobsters, a gangster's wife, a boxer and two small-time criminals.",
+        "genre": "Crime, Drama",
+        "rating": 8.8,
+        "year": "1994",
+        "country":"INDIA",
+        "photo": "https://images.app.goo.gl/UX28GLy1GMvohQCr7"
+      },
+      {
+        "id": 4,
+        "name": "Forrest Gump ",
+        "descripton": "Forrest, a man with low IQ, recounts the early years of his life when he found himself in the middle of key historical events. All he wants now is to be reunited with his childhood sweetheart, Jenny.",
+        "genre": "Drama, Romance",
+        "rating": 8.4,
+        "year": "1994",
+        "country":"INDIA",
+        "photo": "https://images.app.goo.gl/6R1zNbYMpsa2RQZ67"
+      },
+      {
+        "id": 5,
+        "name": "Falling Down",
+        "descripton": "An ordinary man frustrated with the various flaws he sees in society begins to psychotically and violently lash out against them.",
+        "genre": "Action, Crime, Drama",
+        "rating": 7.6,
+        "year": "1993",
+        "country":"USA",
+        "photo": "https://images.app.goo.gl/5YfzB9qGbbsopeT4A"
+      },
+      {
+        "id": 6,
+        "name": "The social network",
+        "descripton": "As Harvard student Mark Zuckerberg creates the social networking site that would become known as Facebook, he is sued by the twins who claimed he stole their idea, and by the co-founder who was later squeezed out of the business.",
+        "genre": "Drama, Biography, History",
+        "rating": 7.7,
+        "year": "2010",
+        "country":"USA",
+        "photo": "https://images.app.goo.gl/LNkr2rhzukM9smdg7"
+      },
+      {
+        "id": 7,
+        "name": "Pirates of Silicon Valley",
+        "descripton": "This is a semi-humorous biographical film about the men who made the world of technology what it is today, their struggles during college, the founding of their companies, and the ingenious actions they took to build up the global corporate empires of Apple Computer Corporation and Microsoft Inc.",
+        "genre": "Drama, Biography, History",
+        "rating": 7.2,
+        "year": "1999",
+        "country":"INDIA",
+        "photo": "https://images.app.goo.gl/HEJHoVjDpNkTfY557"
+      },
+      {
+        "id": 8,
+        "name": "Free Guy",
+        "descripton": "A bank teller discovers that he's actually an NPC inside a brutal, open world video game.",
+        "genre": "Action, Adventure",
+        "rating": 7.4,
+        "year": "2021",
+        "country":"UAE",
+        "photo": "https://images.app.goo.gl/4akM2sMWsqqzeFuD6"
+      },
+      {
+        "id": 9,
+        "name": "Interstellar",
+        "descripton": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+        "genre": "Adventure, Drama, Sci-Fi",
+        "rating": 8.6,
+        "year": "2014",
+        "country":"USA",
+        "photo": "https://images.app.goo.gl/tpKfwhNM2mTt2ngcA"
+      },
+      {
+        "id": 10,
+        "name": "Cinderella 2021",
+        "descripton": "A modern movie musical with a bold take on the classic fairy tale. Our ambitious heroine has big dreams and with the help of her fab Godmother, she perseveres to make them come true.",
+        "genre": "Adventure, Comedy, Family",
+        "rating": 4.2,
+        "year": "2021",
+        "photo": "https://images.app.goo.gl/GaLA5xkcgYNm6yPbA"
+      },
+      {
+        "id": 11,
+        "name": "The Conjuring",
+        "descripton": "In 1970, paranormal investigators and demonologists Lorraine (Vera Farmiga) and Ed (Patrick Wilson) Warren are summoned to the home of Carolyn (Lili Taylor) and Roger (Ron Livingston) Perron. The Perrons and their five daughters have recently moved into a secluded farmhouse, where a supernatural presence has made itself known. Though the manifestations are relatively benign at first, events soon escalate in horrifying fashion, especially after the Warrens discover the house's macabre history.",
+        "genre": "Horror",
+        "rating": 7.5,
+        "year": "2013",
+        "country":"INDIA",
+        "photo": "https://images.app.goo.gl/C8K8SG65UKv5C9xu9"
+      },
+      {
+        "id": 12,
+        "name": "Fight Club",
+        "descripton": "Discontented with his capitalistic lifestyle, a white-collared insomniac forms an underground fight club with Tyler, a careless soap salesman. The project soon spirals down into something sinister.",
+        "genre": "Thriller, Drama",
+        "rating": 8.8,
+        "year": "1999",
+        "country":"UAE",
+        "photo": "https://images.app.goo.gl/9Xd1S1hYUE9sqeQL6"
+      },
+      {
+        "id": 13,
+        "name":"Harry Potter and the Philosopher's Stone",
+        "descripton":"Adaptation of the first of J.K. Rowling's popular children's novels about Harry Potter, a boy who learns on his eleventh birthday that he is the orphaned son of two powerful wizards and possesses unique magical powers of his own. He is summoned from his life as an unwanted child to become a student at Hogwarts, an English boarding school for wizards. There, he meets several friends who become his closest allies and help him discover the truth about his parents' mysterious deaths.",
+        "genre":"Fantasy, Family",
+        "rating": 7.6,
+        "year":"2001",
+        "country":"UK",
+        "photo":"https://static.wikia.nocookie.net/harrypotter/images/f/fb/PS_photo.jpg/revision/latest?cb=20180318153750"
+      },
+      {
+        "id": 14,
+        "name": "Avatar",
+        "descripton": "On the lush alien world of Pandora live the Na'vi, beings who appear primitive but are highly evolved. Because the planet's environment is poisonous, human/Na'vi hybrids, called Avatars, must link to human minds to allow for free movement on Pandora.",
+        "genre": "Action, Fantasy, Sci-Fi, Adventure",
+        "rating": 7.8,
+        "year": "2009",
+        "country":"USA",
+        "photo": "https://moviephotos2.com/images/703197-b.jpg"
+      },
+      {
+        "id": 15,
+        "name":"Get Out",
+        "descripton":"Get Out is a bizarre movie about the events that happen to a guy named Chris who goes to visit his girlfriend’s parents over a weekend.",
+        "genre":"Horror, Thriller",
+        "rating":7.7,
+        "year":"2017",
+        "country":"INDIA",
+        "photo":"https://images.app.goo.gl/jhLvEf5GK28oewdM9"
+      },
+      {
+        "id": 16,
+        "name":"Five Feet Apart",
+        "descripton":"Five Feet Apart Summary F ive Feet Apart is a novel about Stella Grant and Will Newman, two teenagers with cystic fibrosis who fall in love.",
+        "genre":"Romance, Drama",
+        "rating":7.2,
+        "year":"2019",
+        "country":"UK",
+        "photo":"https://images.app.goo.gl/1qWgUe7nCLQjGJeR6"
+      },
+      {
+        "id": 17,
+        "name":"Chemical Hearts",
+        "descripton":"Chemical Hearts’ is the story of a teenager who falls in love for the first time, only to realize that it is not as magnificent and fairy-tale-ish as it is shown in the movies.",
+        "genre":"Romance, Teenage Limbo",
+        "rating":6.3,
+        "year":"2020",
+        "country":"USA",
+        "photo":"https://images.app.goo.gl/bhMv8SPdpRdaSht19"
+      },
+      {
+        "id": 18,
+        "name":"Gone Girl",
+        "descripton":"With his wife's disappearance having become the focus of an intense media circus, a man sees the spotlight turned on him when it's suspected that he may not be innocent.",
+        "genre":"Mystery, Thriller, Drama",
+        "rating":8.1,
+        "year":"2014",
+        "country":"INDIA",
+        "photo":"https://images.app.goo.gl/y3bsrA9Abr3V26bU9"
+      },
+      {
+        "id": 19,
+        "name": "The Accountant",
+        "descripton": "As a math savant uncooks the books for a new client, the Treasury Department closes in on his activities, and the body count starts to rise.",
+        "genre": "Crime, Drama, Thriller, Action",
+        "rating": 7.3,
+        "year": "2016",
+        "country":"USA",
+        "photo": "https://image.tmdb.org/t/p/original/vnEDk1Dr2FqrEaLjD437NYAjYVu.jpg"
+      },
+      {
+        "id": 20,
+        "name": "How to Steal a Million",
+        "descripton": "The daughter of an art forger teams up with a burglar to steal one of her father's forgeries and protect his secret.",
+        "genre": "Crime, Comedy, Romance",
+        "rating": 7.6,
+        "year": "1966",
+        "country":"UAE",
+        "photo": "https://image.tmdb.org/t/p/original/jQuBlzvWABKIqZSin3cpPn9r7br.jpg"
+      },
+      {
+        "id": 21,
+        "name": "Austenland",
+        "descripton": "Obsessed with Pride and Prejudice (1995), a woman travels to a Jane Austen theme park in search for her perfect gentleman.",
+        "genre": "Comedy, Romance",
+        "rating": 6.3,
+        "year": "2013",
+        "country":"UK",
+        "photo": "https://www.reellifewithjane.com/wp-content/uploads/2014/02/Austenland-photo.jpg"
+      },
+      {
+        "id": 22,
+        "name": "Ever After: A Cinderella Story",
+        "descripton": "The Brothers Grimm arrive at the home of a wealthy Grande Dame who speaks of the many legends surrounding the fable of the cinder girl before telling the 'true' story of her ancestor.",
+        "genre": "Drama, Comedy, Romance",
+        "rating": 7.0,
+        "year": "1998",
+        "country":"INDIA",
+        "photo": "https://4.bp.blogspot.com/-Bdje-0eunIo/Ud2psOtsrFI/AAAAAAAAAU8/I8k2t53AcEs/s1600/EverAfter-1998-movie-photo.jpg"
+      },
+      {
+        "id": 23,
+        "name": "No Time to Die",
+        "descripton": "James Bond has left active service. His peace is short-lived when Felix Leiter, an old friend from the CIA, turns up asking for help, leading Bond onto the trail of a mysterious villain armed with dangerous new technology.",
+        "genre": "Action, Adventure, Thriller",
+        "rating": 7.6,
+        "year": "2021",
+        "country":"INDIA",
+        "photo": "https://www.bollywoodhungama.com/wp-content/uploads/2021/09/No-Time-To-Die-to-be-the-FIRST-James-Bond-film-to-release-in-3D-expected-to-be-the-BIGGEST-Hollywood-release-post-pandemic-in-India.jpg"
+      },
+      {
+        "id": 24,
+        "name": "Dune",
+        "descripton": "Feature adaptation of Frank Herbert's science fiction novel, about the son of a noble family entrusted with the protection of the most valuable asset and most vital element in the galaxy.",
+        "genre": "Action, Adventure, Drama",
+        "rating": 8.3,
+        "year": "2021",
+        "country":"USA",
+        "photo": "https://pics.filmaffinity.com/Dune-756528084-large.jpg"
+      },   
+      {
+        "id": 25,
+        "name": "The Lion King",
+        "descripton": "After the murder of his father, a young lion prince flees his kingdom only to learn the true meaning of responsibility and bravery.",
+        "genre": "Animation, Adventure, Drama",
+        "rating": 6.8,
+        "year": "2019",
+        "country":"USA",
+        "photo": "https://lumiere-a.akamaihd.net/v1/images/p_thelionking2019_19732_c1561d41.jpeg"
+      }
+    ]
+  
